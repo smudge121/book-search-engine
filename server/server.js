bf394dff8ac6fa20f0d0a8ab97cbb2ec08c13e76
Jishii <33364545+smudge121@users.noreply.github.com>
2023-02-01 20:30:53 -0800

    server auth and create index for schemas

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
+const { join } = require("path");
+const { authMiddleware } = require("./utils/auth.js");
+
+const { typeDefs, resolvers } = require("./schemas");
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +25,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "client", "build", "index.html"));
 });
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => app.listen(PORT));
+};
+
+startApolloServer(typeDefs, resolvers);
